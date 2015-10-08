cdaca3467cdfc4ee61d5e2b3e3fbdb6d9a8a5843
kushal <kushaldeveloper@gmail.com>
2015-10-08 18:28:00 -0400

    added some pop up code
    thanks to https://stackoverflow.com/questions/22909329/universal-apps-messagebox-the-name-messagebox-does-not-exist-in-the-current/26033086#26033086

--- EightySeven.UWP/MainPage.xaml.cs
+++ EightySeven.UWP/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,37 @@ namespace EightySeven.UWP
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void ButtonSubmitTapped(object sender, RoutedEventArgs e)
+        {
+            MessageDialog msgbox = new MessageDialog("Would you like to greet the world with a \"Hello, world\"?", "My App");
+
+            msgbox.Commands.Clear();
+            msgbox.Commands.Add(new UICommand { Label = "Yes", Id = 0 });
+            msgbox.Commands.Add(new UICommand { Label = "No", Id = 1 });
+            msgbox.Commands.Add(new UICommand { Label = "Cancel", Id = 2 });
+
+            var res = await msgbox.ShowAsync();
+
+            if ((int)res.Id == 0)
+            {
+                MessageDialog msgbox2 = new MessageDialog("Hello to you too! :)", "User Response");
+                await msgbox2.ShowAsync();
+            }
+
+            if ((int)res.Id == 1)
+            {
+                MessageDialog msgbox2 = new MessageDialog("Oh well, too bad! :(", "User Response");
+                await msgbox2.ShowAsync();
+            }
+
+            if ((int)res.Id == 2)
+            {
+                MessageDialog msgbox2 = new MessageDialog("Nevermind then... :|", "User Response");
+                await msgbox2.ShowAsync();
+            }
+
         }
     }
 }
